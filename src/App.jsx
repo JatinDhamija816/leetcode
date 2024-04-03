a4edee4498a0fb7479ccf975d619c46adb042cf5
JatinDhamija816 <jdhamija816@gmail.com>
2024-04-03 16:03:33 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [leetcode, setLeetcode] = useState([])
+
+  useEffect(() => {
+    axios.get('https://leetcodestats.cyclic.app/JatinDhamija816/')
+      .then((res) => {
+        setLeetcode(res.data)
+      }).catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
+  return (
+    <>
+      <p>Ranking: {leetcode.ranking}</p>
+      <p>Total Solved Question: {leetcode.totalSolved}</p>
+      <p>Contribution Points: {leetcode.contributionPoints}</p>
+    </>
+  )
+}
+export default App
